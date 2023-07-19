ba6de3901240444b4340d9642a0ec92fc875a31d
JustA7 <vophong2205@gmail.com>
2023-07-19 07:48:39 +0700

    update route for signin

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import Header from "./components/Header"
-
+import { Routes, Route, useLocation } from 'react-router-dom';
+import SignIn from "./pages/signIn"
 
 export default function App() {
+  const location = useLocation();
   return (
     <div className="w-full">
-      <Header />  
-      
+      {location.pathname!== "/signin" && <Header />  }
+      <Routes>
+        
+        <Route path='/signin' element={<SignIn />} />
+      </Routes>
     </div>
   )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 import { ThemeProvider } from "@material-tailwind/react";
@@ -9,7 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
