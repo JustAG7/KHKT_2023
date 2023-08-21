86aab36e7e88d6c87dd0af80d179674225d9ce67
JustA7 <vophong2205@gmail.com>
2023-08-22 02:12:46 +0700

    update server

---
+++ server/routes/ApiRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const Auth = require("./AuthRoutes");
+const userRoutes = require("./UserRoutes");
+const departmentRoutes = require("./DepartmentRoutes");
+const EventRoutes = require("./EventRoutes");
+
+router.use("/users", userRoutes);
+router.use("/auth", Auth);
+router.use("/departments", departmentRoutes);
+router.use("/events", EventRoutes);
+
+module.exports = router;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,2 +1,8 @@
+const apiRoute = require("./ApiRoutes");
 
+function routes(app) {
+    app.use("/api", apiRoute);
+}
+
+module.exports = routes;
 
