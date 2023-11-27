0d453d8ae7463525f6625e6495d2e8d5dfcba50d
JustAG7 <vophong2205@gmail.com>
2023-11-28 01:28:54 +0700

    update whatever the fuck UN want

--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -1,57 +1,54 @@
 import EventCard from "../components/EventCard";
-import Cookies from 'js-cookie'
+import Cookies from "js-cookie";
 import { useNavigate } from "react-router-dom";
 import AddEvent from "../components/AddEventDialog";
 import { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
 
-
 export default function Events() {
   const navigate = useNavigate();
   const [events, setEvents] = useState([]);
   const ipAddress = window.location.hostname;
   useEffect(() => {
-    if(!Cookies.get('token')){
-      navigate('/signin')
+    if (!Cookies.get("token")) {
+      navigate("/signin");
     }
-  })
+  });
 
   useEffect(() => {
-    axios.get(`http://${ipAddress}:1991/api/events/`, { headers: { Authorization: `Bearer ${Cookies.get('token')}` } })
-      .then(res => {
+    axios
+      .get(`http://${ipAddress}:1991/api/events/`, {
+        headers: { Authorization: `Bearer ${Cookies.get("token")}` },
+      })
+      .then((res) => {
         setEvents(res.data);
         console.log(res.data);
       })
-      .catch(err => {
+      .catch((err) => {
         alert(err);
       });
   }, []);
   return (
-
     <div>
-      {Cookies.get('role') !== "member" &&
+      {Cookies.get("role") !== "member" && (
         <div className="w-max space-x-4">
-          
-          <AddEvent />
           <br></br> <br></br>
         </div>
-      }
+      )}
 
       <div className="flex space-x-10">
-          {events.map((event) => (
-            
-            <EventCard
-              key={event._id}
-              name={event.name}
-              description={event.description}
-              date={event.date}
-              password={event.password}
-              id={event._id}
-              isAccepted={event.participants.includes(Cookies.get('id'))}
-            />
-            
-          ))}
+        {events.map((event) => (
+          <EventCard
+            key={event._id}
+            name={event.name}
+            description={event.description}
+            date={event.date}
+            password={event.password}
+            id={event._id}
+            isAccepted={event.participants.includes(Cookies.get("id"))}
+          />
+        ))}
       </div>
     </div>
   );
